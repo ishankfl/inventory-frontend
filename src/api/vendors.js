7d366b7faf387b734729a601125f588b602da0cb
ishankfl <ishankafle5@gmail.com>
2025-07-18 10:45:09 +0545

    vendor-apis-added

---
+++ src/api/vendors.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { server } from './server';
+import { authHeader } from '../utils/tokenutils';
+
+// Get all vendors
+export const getAllVendors = () => {
+  return axios.get(`${server}/api/Vendor`, authHeader());
+};
+
+// Get a single vendor by ID
+export const getVendorById = (id) => {
+  return axios.get(`${server}/api/Vendor/${id}`, authHeader());
+};
+
+// Add a new vendor
+export const addVendor = (name, email) => {
+  return axios.post(`${server}/api/Vendor`, { name, email }, authHeader());
+};
+
+// Update a vendor
+export const updateVendor = (id, name, email) => {
+  return axios.put(`${server}/api/Vendor/${id}`, { name, email }, authHeader());
+};
+
+// Delete a vendor by ID
+export const deleteVendorById = (id) => {
+  return axios.delete(`${server}/api/Vendor/${id}`, authHeader());
+};
