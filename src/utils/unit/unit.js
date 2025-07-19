1bd9bf059d3ede1570f8b3335bf1d3f4a7648530
ishankfl <ishankafle5@gmail.com>
2025-07-19 11:20:44 +0545

    units-added

---
+++ src/utils/unit/unit.js
@@ -0,0 +1,32 @@
+const productUnits = [
+    // Weight-related units
+    "mg",   // Milligram
+    "cg",   // Centigram
+    "dg",   // Decigram
+    "g",    // Gram
+    "dag",  // Dekagram
+    "hg",   // Hectogram
+    "kg",   // Kilogram
+    "q",    // Quintal
+    "MT",   // Metric Ton
+    "T",    // Metric Ton (alternative)
+    "lb",   // Pound
+    "oz",   // Ounce
+
+    // Common real-world sale units
+    "piece", // Single item
+    "pc",    // Piece (abbreviation)
+    "unit",  // General unit
+    "pkt",   // Packet
+    "box",   // Box
+    "bag",   // Bag
+    "btl",   // Bottle
+
+    // Liquid/Volume based (for weight-equivalent sale)
+    "ml",    // Milliliter
+    "l",     // Liter
+    "L"      // Liter (capital)
+];
+
+
+export default productUnits;
