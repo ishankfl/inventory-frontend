9974cbba4dcec8e9f7d640b2c1bb89df349bf264
ishankfl <ishankafle5@gmail.com>
2025-07-26 16:43:50 +0545

    firebaseconfig-added

---
+++ src/components/common/Message.jsx
@@ -0,0 +1,17 @@
+const Message = ({ notification }) => {
+    return (
+        <>
+            <div id="notificationHeader">
+                {notification.image && (
+                    <div id="imageContainer">
+                        <img src={notification.image} width={100} />
+                    </div>
+                )}
+                <span>{notification.title}</span>
+            </div>
+            <div id="notificationBody">{notification.body}</div>
+        </>
+    );
+};
+
+export default Message;
